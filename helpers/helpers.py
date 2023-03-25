bdd80d06dbe0b60e21b423267c22c4980f027b13
Leônidas Drawanz Ferreira <leonidas.drawanz@gmail.com>
2023-03-25 20:00:58 -0300

    feat: creating the handle_response

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -15,3 +15,8 @@ def combine_prompts(df, query_prompt):
     definition = create_table_definition(df)
     query_init_string = f"### A query to answer: {query_prompt}\nSELECT"
     return definition+query_init_string
+
+
+def handle_response(response):
+    query = response['choices'][0]['text']
+    return 'SELECT'+query
