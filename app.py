a95c88506d8887dd7e9485652b50d7f4622967fb
Leônidas Drawanz Ferreira <leonidas.drawanz@gmail.com>
2023-03-25 20:03:19 -0300

    feat: creating a main function in the app.py

--- app.py
+++ app.py
@@ -5,37 +5,33 @@ from dotenv import load_dotenv
 load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy import text
-from helpers.helpers import combine_prompts, prompt_input
+from helpers.helpers import combine_prompts, prompt_input, handle_response
 
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-df = pd.read_csv("sales_data_sample.csv")
-# print(df.groupby("QTR_ID").sum()['SALES'])
-
-
 # temporary db in ram, pushing df into the temp db
 temp_db = create_engine('sqlite://', echo=False)
+df = pd.read_csv("sales_data_sample.csv")
 data = df.to_sql(name = 'Sales', con=temp_db)
 
 
-# with temp_db.connect() as conn:
-#     result = conn.execute((text("SELECT * FROM Sales")))
-#     rows = result.fetchall()
-#     print(rows)
+def main():
+    nlp_text = prompt_input()
 
-nlp_text = prompt_input()
+    response = openai.Completion.create(
+        model='text-davinci-002',
+        prompt=combine_prompts(df, nlp_text),
+        temperature=0,
+        max_tokens=150,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=['#', ';']
+    )
 
+    with temp_db.connect() as conn:
+        result = conn.execute((text(handle_response(response))))
+        rows = result.fetchall()
+        print(rows)
 
-response = openai.Completion.create(
-    model='text-davinci-002',
-    prompt=combine_prompts(df, nlp_text),
-    temperature=0,
-    max_tokens=150,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=['#', ';']
-)
-
-print(response)
